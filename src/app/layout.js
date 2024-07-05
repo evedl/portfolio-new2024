ef16161f85e6b8110f3606a7ee852e655f8c843c
Evy <82066881+evedl@users.noreply.github.com>
2024-07-05 16:36:37 -0600

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,15 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portafolio de Evedl",
-  description: "Eve.exe",
+    title: "Portafolio de Evedl",
+    description: "Eve.exe",
+    icons: ['/favicon.ico'],
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
